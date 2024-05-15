66a9c8971f782b1ebca6367213b33bbe11affe29
Enesphlvn <pehlivan.enes53@gmail.com>
2024-05-15 23:28:10 +0300

    Password validation options were configured in the program.cs file

---
+++ AspNetCoreIdentityApp.Web/Extensions/StartupExtensions.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+
+namespace AspNetCoreIdentityApp.Web.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddIdentityWithExtension(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz1234567890_";
+
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+
+            }).AddEntityFrameworkStores<AppDbContext>();
+        }
+    }
+}
