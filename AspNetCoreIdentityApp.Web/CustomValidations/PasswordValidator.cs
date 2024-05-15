cbedfa53ceb1f4727556baef6b1faabd65f6318b
Enesphlvn <pehlivan.enes53@gmail.com>
2024-05-16 00:00:24 +0300

    Custom validation class for the UserName field was added

--- AspNetCoreIdentityApp.Web/CustomValidations/PasswordValidator.cs
+++ AspNetCoreIdentityApp.Web/CustomValidations/PasswordValidator.cs
@@ -11,12 +11,12 @@ namespace AspNetCoreIdentityApp.Web.CustomValidations
 
             if (password.ToLower().Contains(user.UserName.ToLower()))
             {
-                errors.Add(new() { Code = "PasswordNoContainUserName", Description = "Şifre alanı kullanıcı adı içeremez." });
+                errors.Add(new() { Code = "PasswordContainUserName", Description = "Şifre alanı kullanıcı adı içeremez." });
             }
 
             if (password.ToLower().StartsWith("1234"))
             {
-                errors.Add(new() { Code = "PasswordNoContain1234", Description = "Şifre alanı ardışık rakam içeremez." });
+                errors.Add(new() { Code = "PasswordContain1234", Description = "Şifre alanı ardışık rakam içeremez." });
             }
 
             if (errors.Any())
