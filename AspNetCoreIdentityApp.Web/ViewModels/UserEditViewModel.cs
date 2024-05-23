18b279159786e6d3ead27f008e4cec216aa60615
Enesphlvn <pehlivan.enes53@gmail.com>
2024-05-23 19:31:20 +0300

    UserEditViewModel class created

--- AspNetCoreIdentityApp.Web/ViewModels/UserEditViewModel.cs
+++ AspNetCoreIdentityApp.Web/ViewModels/UserEditViewModel.cs
@@ -1,7 +1,32 @@
-﻿namespace AspNetCoreIdentityApp.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
 {
     public class UserEditViewModel
     {
+        [Required(ErrorMessage = "Kullanıcı adı alanı boş bırakılamaz")]
+        [Display(Name = "Kullanıcı adı: ")]
+        public string UserName { get; set; } = null!;
 
+        [EmailAddress(ErrorMessage = "Email formatı yanlış.")]
+        [Required(ErrorMessage = "Email alanı boş bırakılamaz.")]
+        [Display(Name = "Email: ")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Telefon numarası alanı boş bırakılamaz.")]
+        [Display(Name = "Telefon: ")]
+        public string Phone { get; set; } = null!;
+
+        [Display(Name = "Doğum tarihi: ")]
+        public string? BirthDate { get; set; }
+
+        [Display(Name = "Şehir: ")]
+        public string? City { get; set; }
+
+        [Display(Name = "Profil resmi: ")]
+        public IFormFile? Picture { get; set; }
+
+        [Display(Name = "Cinsiyet: ")]
+        public byte? Gender { get; set; }
     }
 }
